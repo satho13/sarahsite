24ce2759fbce7e32fd00c95f699bb1987bb02678
Steven <neuhaus@seas.upenn.edu>
2018-09-04 19:23:09 -0400

    fixed kun

---
+++ profiles/kun_xue.php
@@ -0,0 +1,17 @@
+<?php
+$topTitle="Masters Student";
+$memberName="Kun Xue";
+$subname="2018 - Present";
+$numDepts=1;
+$deptNames[1]="Nanotechnology";
+$deptLinks[1]="https://masters.nano.upenn.edu/";
+$addressInfo="";
+$emailAddress1="kunxue@";
+$emailaddress2="seas.upenn.edu";
+$numInstitutions=1;
+$institutionNames[1]="Shanghai Jiao Tong University, Shanghai, China";
+$degrees[1]="B.E. in Materials Science and Engineering, 2017";
+
+$picSRC="grouppictures/photoKunXue.jpg";
+$bioText="";
+?>

03cbe83c6ff478824ae278baa954e8562cd2b519
Steven <neuhaus@seas.upenn.edu>
2019-01-29 17:24:09 -0500

    updates announcments re:martin

--- groupAnnouncements.php
+++ groupAnnouncements.php
@@ -1,4 +1,9 @@
-<b>10/31/18:</b><br/>
+<b>11/30/18:</b><br/>
+<b>Congratulations Martin!</b>
+<br><br>
+	Congratulations to Martin Sarott on his Masters graduation from ETH and on being awarded the Willi-Studer prize for the best overall grade. We wish him the best of luck as he starts his PhD!<br><br>
+
+<p></p><b>10/31/18:</b><br/>
 <b>Congratulations Daniel!</b>
 <br><br>
 	Congratulations to Daniel on defending his PhD thesis! We wish him the best as he starts a Postdoc at Princeton.<br><br>

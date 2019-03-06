4c87bd803fc9e571429a60c69560443cd10caedf
Steven <neuhaus@seas.upenn.edu>
2019-03-06 10:52:23 -0500

    actually added sebastian

---
+++ profiles/sebastian_peralta.php
@@ -0,0 +1,19 @@
+<?php
+$topTitle="Undergraduate";
+$memberName="Sebastian Peralta";
+$subname="Class of 2020";
+$numDepts=2;
+$deptNames[1]="Department of Electrical and Systems Engineering (ESE)";
+$deptLinks[1]="http://www.ese.upenn.edu/";
+$deptNames[2]="Department of Physics and Astronomy";
+$deptLinks[2]="http://www.physics.upenn.edu/";
+
+$addressInfo="";
+$emailAddress1="peraltas@";
+$emailaddress2="seas.upenn.edu";
+$numInstitutions=1;
+$institutionNames[1]="University of Pennsylvania";
+$degrees[1]="B.S.E. in Electrical Engineering";
+$picSRC="grouppictures/photoSebastianPeralta.jpg";
+$bioText="";
+?>
